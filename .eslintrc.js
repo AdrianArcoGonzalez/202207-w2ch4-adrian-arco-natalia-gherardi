b6ffc9e1156fd196d307d0a6440d6210ea9adca9
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-07-14 18:18:51 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "import/extensions": ["Error", "always"],
+  },
+};
